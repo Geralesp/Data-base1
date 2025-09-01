7091073c85803442d3c1814b34c60e598ac40286
geraldine <geraldine41@mail.com>
2025-09-01 08:27:11 -0500

    DDL table users

--- beta.sql
+++ beta.sql
@@ -17,4 +17,4 @@ create table users (
 );
 
 
-INSERT INTO users (ide_number,firstname, email, password) VALUE ('18091418','GERALDINE','ESPINOSA','3175136251')   
+INSERT INTO users (ide_number,firstname, email, password) VALUE ('18091418','GERALDINE','ESPINOSA','3175136251') 
